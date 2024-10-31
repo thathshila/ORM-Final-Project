2897bd4b9e5e7da3404b6563e3df8724b58b3a72
Thathshila <thathshilaashanganie9501@gmail.com>
2024-10-31 18:06:36 +0530

    first commit

---
+++ src/main/java/lk/ijse/controller/DashboardFormController.java
@@ -0,0 +1,56 @@
+package lk.ijse.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class DashboardFormController {
+
+    @FXML
+    private Button btnDashboard;
+
+    @FXML
+    private Button btnPayment;
+
+    @FXML
+    private Button btnProgram;
+
+    @FXML
+    private Button btnStudent;
+
+    @FXML
+    private Button btnUser;
+
+    @FXML
+    private Label lblCurrentUser;
+
+    @FXML
+    private Label lblDate;
+
+    @FXML
+    void btnDashboardOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPaymentOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnProgramOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnStudentOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUserOnAction(ActionEvent event) {
+
+    }
+
+}
